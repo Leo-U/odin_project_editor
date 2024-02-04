3ecdfa1f7192714ed81e7542e8ee05ced004f75d
Joel Howard <joelhoward2@gmail.com>
2024-02-03 20:35:38 -0500

    Add save functionality

--- popup.js
+++ popup.js
@@ -3,3 +3,9 @@ document.getElementById('toggle-edit').addEventListener('click', () => {
     browser.tabs.sendMessage(tabs[0].id, {action: "toggleEditMode"});
   });
 });
+
+document.getElementById('save-content').addEventListener('click', () => {
+  browser.tabs.query({active: true, currentWindow: true}, tabs => {
+    browser.tabs.sendMessage(tabs[0].id, {action: "saveContent"});
+  });
+});
