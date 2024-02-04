bbf6e3491fdb92313f20e564d1a54d51cf75d952
Joel Howard <joelhoward2@gmail.com>
2024-02-03 19:00:29 -0500

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,6 +1,16 @@
+// Function to toggle edit mode
+function toggleEditMode() {
+  const content = document.querySelector('.lesson-content'); // Replace '.lesson-content' with the actual selector of the content you want to edit
+  if (content) {
+    content.contentEditable = !content.isContentEditable;
+  }
+}
+
+// Listener for messages from popup.js
 browser.runtime.onMessage.addListener(request => {
   if (request.action === "toggleEditMode") {
-    // Implement editing functionality here
-    
+    toggleEditMode();
   }
 });
+
+// You can include other functions or code needed for your extension here
